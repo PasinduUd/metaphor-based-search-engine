ae7d27548490cf9b86763c5f9fede3075d6aa0fd
Pasindu Udawatta <pasindu.secon@gmail.com>
2023-01-19 09:55:40 +0530

    Reformat

--- API/search.py
+++ API/search.py
@@ -2,6 +2,7 @@ from googletrans import Translator
 import io
 import json
 import string
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.corpus import stopwords
